6158990bd2508a36bd600da3c4b5b192320b113b
Drew Gallagher <drewg2009@gmail.com>
2018-02-14 15:56:03 -0500

    added login screen. using fragment transactions.

--- app/src/main/java/mobileapps/osubardeals/osubardealsapp/MainActivity.java
+++ app/src/main/java/mobileapps/osubardeals/osubardealsapp/MainActivity.java
@@ -1,32 +1,35 @@
 package mobileapps.osubardeals.osubardealsapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        manager = getSupportFragmentManager();
+        LoginFragment loginFragment = new LoginFragment();
+        loadFragment(loginFragment);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    private void loadFragment(Fragment f){
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragmentContainer,f,"LoginFragment");
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @Override
