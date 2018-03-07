d2d1ee97f75fbd5bc3f4af0445015b6ddd04907c
Drew Gallagher <drewg2009@gmail.com>
2018-03-07 13:22:01 -0500

    added sample adapter with recycler view for deals page

---
+++ app/src/main/java/mobileapps/osubardeals/osubardealsapp/Adapters/DealsAdapter.java
@@ -0,0 +1,71 @@
+package mobileapps.osubardeals.osubardealsapp.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import mobileapps.osubardeals.osubardealsapp.R;
+
+/**
+ * Created by drewgallagher on 3/7/18.
+ */
+
+public class DealsAdapter extends RecyclerView.Adapter<DealsAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public LinearLayout ll;
+        public TextView tv;
+        public ViewHolder(LinearLayout ll) {
+            super(ll);
+            this.ll = ll;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public DealsAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public DealsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+
+        LinearLayout dealLayout = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.single_deal, parent, false);
+        TextView tv = (TextView) dealLayout.findViewById(R.id.dealTextView);
+
+        // create a new view
+//        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+//                .inflate(R.layout.my_text_view, parent, false);
+
+        ViewHolder vh = new ViewHolder(dealLayout);
+        vh.tv = tv;
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+
+
+        holder.tv.setText(mDataset[position]);
+
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
